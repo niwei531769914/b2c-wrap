f821df59eba0dfb1a80f1700d2aa4e18739c3d6e
wangpen <931075365@qq.com>
2017-09-06 10:32:50 +0800

    2017/9/6 commit

--- webpack-template/src/scripts/example.jsx
+++ webpack-template/src/scripts/example.jsx
@@ -1,61 +1,61 @@
-
 /*
  * 编辑地址 容器主入口
  */
-import React ,{ Component } from 'react';
-import ReactDOM , { render } from 'react-dom';
-
+import React, {Component} from 'react';
+import ReactDOM, {render} from 'react-dom';
 
 
 class List extends Component {
     render() {
         return (
-        	<div>
-        	<input type="text" defaultValue={this.props.index}/>
-            <span onClick={this.props.delete} data-index={this.props.index}>X</span>
+            <div>
+                <input type="text" defaultValue={this.props.index}/>
+                <span onClick={this.props.delete} data-index={this.props.index}>X</span>
             </div>
         )
     }
 }
+
 class Lists extends Component {
     constructor(props) {
         super(props);
-        this.add=this.add.bind(this);
-        this.delete=this.delete.bind(this);
-        this.state={
-            lists:[]
-        }             
+        this.add = this.add.bind(this);
+        this.delete = this.delete.bind(this);
+        this.state = {
+            lists: []
+        }
     }
 
-    add(){
-        var lists=this.state.lists;
+    add() {
+        var lists = this.state.lists;
         lists.push(
-        	<List key={this.state.lists.length} index={this.state.lists.length} delete={this.delete}/>
+            <List key={this.state.lists.length} index={this.state.lists.length} delete={this.delete}/>
         );
         this.setState(
-        	{lists:lists}
+            {lists: lists}
         )
     }
 
-    delete(e){
-    	var lists=this.state.lists;
-        var index=e.target.getAttribute("data-index");
+    delete(e) {
+        var lists = this.state.lists;
+        var index = e.target.getAttribute("data-index");
         console.log(index)
-        lists.splice(index,1,"");
+        lists.splice(index, 1, "");
         this.setState(
-        	{lists:lists}
+            {lists: lists}
         )
-        
+
     }
 
     render() {
         return (<div>
             <span onClick={this.add}>添加</span>
             {this.state.lists}
-            </div>)
+        </div>)
     }
 }
+
 ReactDOM.render(
     <Lists/>,
-    document.getElementById('lists')
+    document.getElementById('lists');
 );
