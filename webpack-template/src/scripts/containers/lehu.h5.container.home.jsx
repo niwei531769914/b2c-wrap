5fc10497a616e32116006429e3f776b794e334c5
wangpen <931075365@qq.com>
2017-09-21 11:52:20 +0800

    commit home

--- webpack-template/src/scripts/containers/lehu.h5.container.home.jsx
+++ webpack-template/src/scripts/containers/lehu.h5.container.home.jsx
@@ -23,7 +23,7 @@ class Home extends Component {
         super(props);
 
         this.state = {
-            tablist: [],
+            categoryList: [],
 
         };
         //时间戳
@@ -35,34 +35,15 @@ class Home extends Component {
 
         let that = this;
 
-
-        //绑定事件
-        $('#scroller ul li').on('tap', function () {
-
-            if ($(this).hasClass('active')) {
-                return false;
-            }
-
-            $('#scroller ul li').removeClass('active');
-            $(this).addClass('active');
-            let PositionIndex = document.querySelector('#scroller li[class="active"]');
-            //滚动中间 设为true，顶部设为false
-            that.Scroll.scrollToElement(PositionIndex, 200, false, false);
-
-            //过渡展示右侧页面
-            document.querySelector('#branchList').style.opacity = '0';
-            document.querySelector('#branchList').style.transition = 'opacity .3s linear 0s';
-            setTimeout(function () {
-                document.querySelector('#branchList').style.opacity = '1';
-                setTimeout(function () {
-                    document.querySelector('#branchList').setAttribute('style', 'transform: translate(0px, 0px);');
-                }, 300)
-            }, 300);
-
-        });
-
         //页面初始化数据 显示状态100
         this.apiRequest();
+    }
+
+    componentWillUnmount() {
+
+        //移除事件 避免内存泄漏
+        $('#category ul li').unbind("tap");
+
     }
 
     //请求数据
@@ -77,19 +58,59 @@ class Home extends Component {
         Api(param)
             .done((data) => {
                 if (data.code == 1) {
-                    console.log(data)
+
                     this.setState({
-                        tablist: data.response
+                        categoryList: data.response
                     });
 
-                    that.Scroll = new IScroll('#wrapper',
+                    //左右切换
+                    let TotalLength = parseFloat($('#category ul li').width()) * parseFloat($('#category ul li').length);
+                    $('#category ul').css({
+                        'width': TotalLength + 'px'
+                    });
+
+                    that.Scroll = new IScroll('#category',
                         {
-                            preventDefault: false,
+                            scrollX: true,
+                            scrollY: false,
                             tap: true,
-                            scrollbars: false,
-                            hScroll: true,
-                            hScrollbar: false,
                         });
+
+                    //绑定事件
+                    $('#category ul li').on('tap', function () {
+                        if ($(this).hasClass('active')) {
+                            return false;
+                        }
+                        if ($(this).find('span').hasClass('span_active')) {
+                            return false;
+                        }
+                        $('#category ul li').removeClass('active');
+                        $('#category ul li').find('span').removeClass('span_active');
+                        $(this).addClass('active');
+                        $(this).find('span').addClass('span_active');
+                        let PositionIndex = document.querySelector('#category li[class="active"]');
+                        //滚动中间 设为true，顶部设为false
+                        that.Scroll.scrollToElement(PositionIndex, 200, false, false);
+
+                        //过渡展示右侧页面
+                        document.querySelector('#branchList').style.opacity = '0';
+                        document.querySelector('#branchList').style.transition = 'opacity .3s linear 0s';
+                        setTimeout(function () {
+                            document.querySelector('#branchList').style.opacity = '1';
+                            setTimeout(function () {
+                                document.querySelector('#branchList').setAttribute('style', 'transform: translate(0px, 0px);');
+                            }, 300)
+                        }, 300);
+
+                    });
+
+                    document.querySelector('#category').addEventListener('touchmove', function (e) {
+                        e.preventDefault();
+                    }, false);
+                    document.querySelector('#branchScroll').addEventListener('touchmove', function (e) {
+                        e.preventDefault();
+                    }, false);
+
                 }
                 else {
                     alert(data.msg);
@@ -104,23 +125,38 @@ class Home extends Component {
 
     render() {
         let that = this;
-        const {
-            tablist
-        } = this.state;
+        const {categoryList} = this.state;
         return (
             <div>
                 {/*top*/}
                 <Search/>
                 {/*nav*/}
-                <div id="wrapper">
-                    <ul>  {
-                        tablist instanceof Array && tablist.length > 0 ? tablist.map((item, index) => {
-                            return (
-                                <li>{item.titlename}<span></span></li>
-                            )
-                        }) : null
-                    }
-                    </ul>
+                <div className="category-viewport">
+                    {/*left nameList*/}
+                    <div className="hy-category-tab">
+                        <div id="category">
+                            <ul>
+                                {
+                                    categoryList.map((item, index) => {
+                                        return (index == 0 ? <li key={index} className="active"><a
+                                                href="javascript: void (0)">{item.titlename}<span className="span_active"></span></a></li> :
+                                            <li key={index}><a href="javascript: void (0)">{item.titlename}<span></span></a>
+                                            </li>)
+                                    }, this)
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                    {/*right content*/}
+                    <div className="hy-category-content">
+                        <div id="branchScroll" className="hy-category-content-wrapper">
+                            <div id="branchList">
+                                <div style={{height: '1000px', backgroundColor: 'yellow', width: '100%'}} className="">
+                                    22
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
             </div>
